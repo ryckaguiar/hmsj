4c9d1128ff4c8bef5cb7402f1c4b40a548a0dc7d
ryckaguiar <ryckaguiar@msn.com>
2016-12-01 20:38:11 +0000

    atualizações linguagem

---
+++ src/main/java/hsmj/io/config/LocaleConfiguration.java
@@ -0,0 +1,39 @@
+package hsmj.io.config;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class LocaleConfiguration extends WebMvcConfigurerAdapter{
+    
+    @Bean (name = "localeResolver")
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.forLanguageTag("pt_BR"));
+        return sessionLocaleResolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+    	LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+    	localeChangeInterceptor.setParamName("lang");
+    	registry.addInterceptor(localeChangeInterceptor);
+    }
+    
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("classpath:/application");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }    
+}
